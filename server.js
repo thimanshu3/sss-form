209bb482d8a37076aed7857ad0998503c2cd9b2a
Ekansh Jain <ejekanshjain@gmail.com>
2020-06-22 22:25:24 +0530

    complete backend and fetch

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const MySql = require('./db')
 const Feedback = require('./models/FeedbackModel')
 const sendMail = require('./sendMail')
+const random = require('./random')
 
 const app = express()
 
@@ -15,10 +16,32 @@ app.use(express.static('public'))
 
 app.post('/verify', async (req, res) => {
 
+    if (!req.body.email.includes('@technonjr.org'))
+        return res.status(400).json({ status: 400, message: 'Email ID must be College Email ID!' })
+
+    const foundFeedback = await Feedback.findOne({
+        email: req.body.email
+    })
+    if (foundFeedback)
+        return res.status(400).json({ status: 400, message: 'You have already taken the survey!' })
+
+    const code = random(6)
+
+    sendMail(req.body.email, 'Your Verification Code for Student Satisfaction Survey', `${code}`)
+
+    res.json({ status: 200, message: 'Verification Code Sent To Your Email!', code })
 })
 
 app.post('/feedback', async (req, res) => {
-    res.json({ status: 200, message: 'Done' })
+    try {
+        await Feedback.create(req.body)
+        res.status(201).json({ status: 201 })
+    } catch (err) {
+        if (err.name === 'SequelizeUniqueConstraintError')
+            return res.status(400).json({ status: 400, message: 'You have already taken the survey!' })
+        console.log(err)
+        res.status(500).json({ status: 500, message: err.toString() })
+    }
 })
 
 app.listen(process.env.PORT || 5000, () => console.log(`Server Started on port ${process.env.PORT || 5000}...`))
